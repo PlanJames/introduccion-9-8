c63b26dbc78b7e5ec19eb1b57c345316366bf5e0
PlanJames <joselencina0612@gmail.com>
2024-08-09 19:46:18 -0300

    primer commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//naranja: es primitivo
+//void: cuando no devuelve nada, solo para que ejecute el programa
+// las clases pueden ser publicas: se puede ver de todas partes
+//privado se puede modificar desde la misma clase (atributos)
+// ++ el valor mas uno
+//long numero con mas caracteres
+//double acepta decimales de los 2 lados
+// si no aparece master poner: git init
